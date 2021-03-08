fc0e4cb373cf9852efa9e6e41c903b7c7debe15d
Vitor Paiva <vitor.paiva@picpay.com>
2021-03-08 09:52:31 -0300

    Exercicio 1 - Aula sobre strings e print

--- src/Exec0.java
+++ src/Exec0.java
@@ -1,14 +1,18 @@
 import java.util.Locale;
 
-public class Main {
+public class Exec0 {
     public static void main(String[] args){
-        Locale.setDefault(Locale.US);
         int y = 32;
         double x = 10.35784;
+        System.out.println("Ola mundo");
+        System.out.println("Bom dia");
         System.out.println(y);
-        System.out.printf("%.4f%n",x);
+        System.out.println(x);
+        System.out.printf("%.2f%n", x);
+        System.out.printf("%.4f%n", x);
         System.out.println("RESULTADO = " + x + " METROS");
-        System.out.printf("RESULTADO = %.2f %.4f metros%n",x, x);
+        System.out.printf("RESULTADO = %.2f METROS%n", x);
+
         String nome = "Maria";
         int idade = 31;
         double renda = 4000.0;
